307395989e2af6d846602a36154382a15676c06c
Tuz <youngtrips@gmail.com>
2012-07-05 09:28:47 +0800

    initialize commit

---
+++ servo/servo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import serial
+import struct
+import sys
+
+SERIAL_PORT = "COM3"
+
+def main(op_dir, op_val):
+	ser = serial.Serial(SERIAL_PORT, 9600, timeout=1)
+	msg = struct.pack('BB', op_dir, op_val)
+	print ser.write(msg)
+	ser.close()
+
+def usage():
+	print 'Usage: %s DIRECTION OFFSET' % (sys.argv[0])
+	print 'DIRECTION: 1=left, 2=right'
+
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		usage()
+		sys.exit(0)
+	op_dir = int(sys.argv[1])
+	op_val = int(sys.argv[2])
+	main(op_dir, op_val)
+
---
+++ servo/servo_control/servo_control.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>
+
+#define DIR_L 0x1
+#define DIR_R 0x2
+
+#define MIN_D  0
+#define MAX_D  160
+
+#define SERVO_PIN 9
+
+Servo servo;
+int pos = 80;
+
+byte val;
+byte dir;
+
+void setup()
+{
+    Serial.begin(9600);
+    servo.attach(SERVO_PIN);
+    servo.write(pos);
+}
+
+void loop()
+{
+    if (Serial.available() > 0) {
+        dir = Serial.read();
+        val = Serial.read();
+		switch(dir) {
+			case DIR_L:
+				if (pos - val >= MIN_D) {
+					pos -= val;
+					servo.write(pos);
+                                        Serial.print(pos);
+				}
+				break;
+			case DIR_R:
+				if (pos + val <= MAX_D) {
+					pos += val;
+					servo.write(pos);
+                                        Serial.print(pos);
+				}
+				break;
+		}
+    }
+}
+
+
